3bc6b6192cc91dc17eb0130fd93b8b69fbdac739
Bartosz Spyrko-Smietanko <bspyrkos@redhat.com>
2019-12-11 14:14:36 +0000

    [18065] remove Multiple JSF Applications found on same ClassLoader WARN

--- impl/src/main/java/javax/faces/CurrentThreadToServletContext.java
+++ impl/src/main/java/javax/faces/CurrentThreadToServletContext.java
@@ -245,8 +245,8 @@ final class CurrentThreadToServletContext {
                     if (matchingClassLoader != null) {
                         // If the match was found on a previous iteration...
                         if (matchingKey != null) {
-                            LOGGER.log(WARNING,
-                                "Multiple JSF Applications found on same ClassLoader.  Unable to safely determine which FactoryFinder instance to use. Defaulting to first match.");
+//                            LOGGER.log(WARNING,
+//                                "Multiple JSF Applications found on same ClassLoader.  Unable to safely determine which FactoryFinder instance to use. Defaulting to first match.");
                             break;
                         }
                         matchingKey = currentKey;
